54634ac401f2138e970535b01b010333b168a69c
Victors1967 <victorsmirnov@me.com>
2021-05-05 18:15:36 +0300

    edit url

--- src/app/service/posts.service.ts
+++ src/app/service/posts.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+// const REST_API = 'http://localhost:8080/news';
+const REST_API = 'https://parser-weather.herokuapp.com/news';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  private postUrl = "http://localhost:8080/news";
+  private postUrl = REST_API;
 
   constructor(private http: HttpClient) { }
 
